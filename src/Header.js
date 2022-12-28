402e410dfc253f2d76a9c6ee82d8cfb8150ab5be
islam alghoul <islamalghoul1997>
2022-12-28 13:20:32 +0200

    401-images-app

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css'
+
+export default function Header() {
+
+  return (
+    <div className='FirstDiv'>
+    <div className='div2'>
+    <h1 className='FirstLine'> 401-images-app </h1>
+    </div>
+    <p className='ThirdLine'> A website to provide you with 3 natural images </p>
+    </div>
+  )
+}
